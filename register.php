ba52b580023d61334f00df9f6caa8f6d3d339e89
Braddy <braddy@fitness-academie.fr>
2023-10-04 17:00:37 +0200

    Ajout logout.php et modif register

--- register.php
+++ register.php
@@ -38,17 +38,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ShopNet - Inscription</title>
-    <link rel="stylesheet" href="assets/css/style.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-    <script src="assets/js/index.js"></script>
-    <script src="https://polyfill.io/v3/polyfill.min.js?features=default" defer></script>
+    <?php require_once('Components/_head.php'); ?>
+    <title>ShopNet - Register</title>
 </head>
 
 <body>
+    <?php include('nav.php'); ?>
+    <div style="margin-top: 20px;"></div>
+
     <form action="register.php" method="POST">
         <div class="container">
             <div class="form-group">
