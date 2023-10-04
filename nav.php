de4921933ac36f50855fe28eb626ace751485fc9
ematteiIpssi <146735867+ematteiIpssi@users.noreply.github.com>
2023-10-04 17:09:16 +0200

    nav.php a jour -> produit

--- nav.php
+++ nav.php
@@ -1,16 +1,16 @@
     <nav class="navbar navbar-expand-lg bg-body-tertiary" style="background-color: green !important;">
       <div class="container-fluid">
-        <a class="navbar-brand" href="#" style="color:white;">ShopNet</a>
+        <a class="navbar-brand" href="index.php" style="color:white;">ShopNet</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNavDropdown">
           <ul class="navbar-nav">
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#" style="color:white;">Home</a>
+              <a class="nav-link active" aria-current="page" href="index.php" style="color:white;">Home</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="produit.html" style="color:white;">Produits</a>
+              <a class="nav-link" href="produit.php" style="color:white;">Produits</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="#" style="color:white;">Nous contacter</a>
