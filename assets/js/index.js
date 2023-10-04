8824871b8d3e3ee669374e7cc725b8bb6ff22f4f
Braddy <braddy@fitness-academie.fr>
2023-10-04 15:37:29 +0200

    Modification index et js

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,83 +4,35 @@ addEventListener('load', () => {
     const confidential = localStorage.getItem('confidential');
     const doneConfidential = document.getElementById('doneConfidential');
     const divConfidential = document.getElementById('confidential');
-    function hiddenConfidential() {
-        divConfidential.classList.add('hidden');
-    }
-    if (confidential) {
+
+
+    if (confidential === "hidden") {
         hiddenConfidential()
     } else {
-        localStorage.setItem('confidential', false)
+        localStorage.setItem('confidential', "notHidden");
+
     }
 
     doneConfidential.addEventListener('click', function () {
-        localStorage.setItem('confidential', true);
+        localStorage.setItem('confidential', "hidden");
         hiddenConfidential()
     })
 
-    /** DarkMode */
-    document.body.style.backgroundColor = localStorage.getItem("darkmode", document.body.style.backgroundColor)
-    const mode = document.getElementById("buttonDarkMode");
-    mode.addEventListener('click', () => {
-        if (document.body.style.backgroundColor == '') {
-            document.body.style.backgroundColor = "#000000"
-        } else {
-            document.body.style.backgroundColor = ''
-        }
-        localStorage.setItem("darkmode", document.body.style.backgroundColor)
-    })
-
-
-    /** affichage produit */
-    const productList = document.getElementById("productList")
-    fetch('./db.json')
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-            data.forEach(product => {
-                let element = document.createElement("li")
-                let p1 = document.createElement("p")
-                let p2 = document.createElement("p")
-                let p3 = document.createElement("p")
-                let p4 = document.createElement("p")
-
-                p1.textContent = product.nom + ' - ' + product.prix + '€'
-                p2.textContent = product.description
-                // p3.textContent = product.prix
-                p4.textContent = product.quantite
-                element.appendChild(p1)
-                element.appendChild(p2)
-                element.appendChild(p3)
-                element.appendChild(p4)
-                productList.appendChild(element)
-            })
-        })
-
-    /** Google map */
-    let map;
-
-    async function initMap() {
-        // The location of Uluru
-        const position = { lat: -25.344, lng: 131.031 };
-        // Request needed libraries.
-        //@ts-ignore
-        const { Map } = await google.maps.importLibrary("maps");
-        const { AdvancedMarkerView } = await google.maps.importLibrary("marker");
-
-        // The map, centered at Uluru
-        map = new Map(document.getElementById("map"), {
-            zoom: 4,
-            center: position,
-            mapId: "DEMO_MAP_ID",
-        });
-
-        // The marker, positioned at Uluru
-        const marker = new AdvancedMarkerView({
-            map: map,
-            position: position,
-            title: "Uluru",
-        });
+    function hiddenConfidential() {
+        divConfidential.style.display = "none"
     }
 
-    initMap();
+    /** DarkMode */
+    // document.body.style.backgroundColor = localStorage.getItem("darkmode", document.body.style.backgroundColor)
+    // const mode = document.getElementById("mode");
+    // mode.addEventListener('click', () => {
+    //     if (document.body.style.backgroundColor == '') {
+    //         document.body.style.backgroundColor = "#000000"
+    //     } else {
+    //         document.body.style.backgroundColor = ''
+    //     }
+    //     localStorage.setItem("darkmode", document.body.style.backgroundColor)
+    // })
+
+
 })
---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>ShopNet</title>
+  <link rel="stylesheet" href="assets/css/style.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+  <script src="assets/js/index.js"></script>
+  <script src="https://polyfill.io/v3/polyfill.min.js?features=default" defer></script>
+</head>
+
+<body style="background-color: grey;">
+  <?php include('nav.html') ?>
+  <div id="confidential">
+    Veuillez accepter notre politique de confidentialité.
+    <button id="doneConfidential">OK</button>
+  </div>
+  <div class="container text-start" style="background-color: whitesmoke;">
+    <div class="row">
+      <div class="col">
+        <img src="img/CC-1200-x-600-Firstline-Workers-Modern-Workplace-Blog-1200x577.jpg" class="img-thumbnail" alt="">
+      </div>
+      <div class="col mx-auto p-2">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras nec viverra erat. Etiam nec lectus sed risus luctus euismod dignissim ac ipsum. Morbi fringilla tincidunt dui in gravida. Donec vitae sem sit amet purus porta pulvinar. Cras tortor felis, tempus sit amet molestie vel, maximus ut diam. Suspendisse vitae lobortis leo. Morbi molestie a risus id egestas. Mauris aliquam, urna malesuada porttitor vehicula, felis diam feugiat mi, sed posuere massa nibh id risus. Quisque iaculis euismod velit, ac vulputate est consequat eu.
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <img src="img/FrontlineWorkers.jpg.jpg" class="img-thumbnail" alt="">
+      </div>
+      <div class="col">
+        <img src="img/FrontlineWorkers.jpg.jpg" class="img-thumbnail" alt="">
+      </div>
+      <div class="col">
+        <img src="img/FrontlineWorkers.jpg.jpg" class="img-thumbnail" alt="">
+      </div>
+      <div class="row">
+        <div class="col">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras nec viverra erat. Etiam nec lectus sed risus luctus euismod dignissim ac ipsum. Morbi fringilla tincidunt dui in gravida. Donec vitae sem sit amet purus porta pulvinar. Cras tortor felis, tempus sit amet molestie vel, maximus ut diam. Suspendisse vitae lobortis leo. Morbi molestie a risus id egestas. Mauris aliquam, urna malesuada porttitor vehicula, felis diam feugiat mi, sed posuere massa nibh id risus. Quisque iaculis euismod velit, ac vulputate est consequat eu.
+
+        </div>
+        <div class="col">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras nec viverra erat. Etiam nec lectus sed risus luctus euismod dignissim ac ipsum. Morbi fringilla tincidunt dui in gravida. Donec vitae sem sit amet purus porta pulvinar. Cras tortor felis, tempus sit amet molestie vel, maximus ut diam. Suspendisse vitae lobortis leo. Morbi molestie a risus id egestas. Mauris aliquam, urna malesuada porttitor vehicula, felis diam feugiat mi, sed posuere massa nibh id risus. Quisque iaculis euismod velit, ac vulputate est consequat eu.
+
+        </div>
+        <div class="col">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras nec viverra erat. Etiam nec lectus sed risus luctus euismod dignissim ac ipsum. Morbi fringilla tincidunt dui in gravida. Donec vitae sem sit amet purus porta pulvinar. Cras tortor felis, tempus sit amet molestie vel, maximus ut diam. Suspendisse vitae lobortis leo. Morbi molestie a risus id egestas. Mauris aliquam, urna malesuada porttitor vehicula, felis diam feugiat mi, sed posuere massa nibh id risus. Quisque iaculis euismod velit, ac vulputate est consequat eu.
+        </div>
+      </div>
+    </div>
+</body>
+
+</html>
