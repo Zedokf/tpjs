a096f7c73df4314469eb4c2ef1b6608bb19015fb
Braddy <braddy@fitness-academie.fr>
2023-10-04 16:36:34 +0200

    Opti

--- login.php
+++ login.php
@@ -40,17 +40,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-    <title> Se connecter</title>
-    <link rel="stylesheet" href="assets/css/style.css">
-
+    <?php require_once('Components/_head.php'); ?>
+    <title>ShopNet</title>
 </head>
 
 <body style="background-color: gray;">
-    <iframe src="nav.html" width="100%"></iframe>
+    <?php include('nav.php'); ?>
+    <div style="margin-top: 20px;"></div>
     <div class="mx-auto p-2" style="background-color: white;width: 25%; border-radius: 20px;">
         <form action="" method="POST">
             <div class="mb-3">
